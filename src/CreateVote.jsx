3db0e125255bde9babd37a8a5e95e4c946a44887
oldmcdonnell <oldmcdonnell@gmail.com>
2024-06-11 11:39:44 -0400

    setting up vote

--- src/CreateVote.jsx
+++ src/CreateVote.jsx
@@ -1,6 +1,34 @@
+import { useContext, useState, useEffect } from "react"
+import { Container, Column } from "react-bootstrap"
+import { AuthContext } from "./context";
+
 /*-
 add candidate
 one vote per person?
 select option one throught number of candidates
+
+
 -*/
 
+function CreateVote() {
+    const { state, dispatch } = useContext(AuthContext);
+    const [votes, setVotes ] = useState(0)
+    const [candidates, setCandidates] = useState()
+    const [rounds, setRounds] = useState()
+
+
+    return (
+        <>
+        <Container>
+            <Column>
+            {/* candidates */}
+            </Column>
+            <Column>
+            {/* votes */}
+            </Column>
+        </Container>
+        </>
+    )
+}
+
+export default CreateVote
