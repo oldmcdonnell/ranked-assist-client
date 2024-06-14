29f4fd7770003b6c2d9ceb10be01e8073f0213ec
oldmcdonnell <oldmcdonnell@gmail.com>
2024-06-14 08:48:31 -0400

    fixing baeurl for main

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
-const baseUrl = "http://127.0.0.1:8000";
-// const baseUrl = "https://ranked-assist-server.fly.dev";
+// const baseUrl = "http://127.0.0.1:8000";
+const baseUrl = "https://ranked-assist-server.fly.dev";
 
 export const getToken = async ({ dispatch, username, password }) => {
   try {
